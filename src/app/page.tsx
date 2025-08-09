be70329733c14a571a9bc116745ded7b513856fc
Clas Olaf Steinbru Andersen <kosecosa@CLASTOP.localdomain>
2025-08-09 18:19:45 +0200

    fix: text fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TypeWriter from "./components/typewriter/typewriter";
 export default function Home() {
   return (
     <div className="text-4xl font-sans grid items-center justify-items-center min-h-screen min-w-screen">
-      <TypeWriter text={"Coming soon..."} /> 
+      <TypeWriter text={"Coming Soon..."} /> 
     </div>
   );
 }
